8b4467fd3d5eabd4abe06777773dc400e8dd182e
Alymbekov <maxim.makarov.1997@mail.ru>
2019-05-22 02:50:35 +0600

    add pagination to blog

--- geoportal/urls.py
+++ geoportal/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from geoportal.views import PostListView, PostDetailView
+from geoportal.views import post_list, PostDetailView
 from django.urls import path, include
 app_name = 'cities'
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('blog-post/', PostListView.as_view(), name="blog-post"),
+    path('blog-post/', post_list, name="blog-post"),
     path('blog-post/<int:pk>/', PostDetailView.as_view(), name="blog-single"),
     path('city/', views.CitiesListView.as_view(), name="city-list"),
     path('city/<int:pk>/', views.CitiesDetailView.as_view(), name="city-detail"),
--- geoportal/views.py
+++ geoportal/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .models import WorldBorder, Post, Tag
 
@@ -19,10 +21,39 @@ def index(request):
     return render(request, 'index.html', {})
 
 
-class PostListView(ListView):
-    template_name = 'blog.html'
-    model = Post
+def post_list(request):
+    search_query = request.GET.get('search', '')
 
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query)| Q(body__icontains=search_query))
+
+    else:
+        posts = Post.objects.all()
+
+    paginator = Paginator(posts,2)
+
+    page_number = request.GET.get('page',1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        previus_url = '?page={}'.format(page.previous_page_number())
+    else:
+        previus_url = ''
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page_object':page,
+        'is_paginated':is_paginated,
+        'next_url': next_url,
+        'previus_url':previus_url
+    }
+
+    return render(request, 'blog.html', context=context)
 
 class PostDetailView(DetailView):
     template_name = 'blog-single.html'
@@ -31,6 +62,7 @@ class PostDetailView(DetailView):
     def get(self, request, pk):
         obj = get_object_or_404(self.model, pk=pk)
         return render(request, self.template_name, context={'objects': obj})
+
 
 
 class TagListView(ListView):
